8abfa90c172141d7fbc8d181dc51243d08fa2299
RidanDev <gianlucanadir@gmail.com>
2019-11-09 20:52:20 +0100

    add comments

--- src/FieldArrayForm.js
+++ src/FieldArrayForm.js
@@ -3,15 +3,17 @@ import { Field, FieldArray, reduxForm } from "redux-form";
 import validate from "./validate";
 import Output from "./Output";
 
-const renderField = ({ label, input, type, meta: { touched, error } }) => (
-  <div>
+const renderField = ({ label, input, type, meta: { touched, error } }) => {
+  console.log(input);
+  //pass the input prop is necessary for it to work
+  return <div>
     <label>{label}</label>
     <div>
       <input {...input} type={type} placeholder={label} />
       {touched && error && <span>{error}</span>}
     </div>
   </div>
-);
+}
 
 const renderMembers = ({ fields }) => (
   <ul>
@@ -20,28 +22,30 @@ const renderMembers = ({ fields }) => (
         Add Member
       </button>
     </li>
-    {fields.map((fieldName, index) => (
-      <li key={index}>
-        <button
-          type="button"
-          title="Remove Member"
-          onClick={() => fields.remove(index)}
-        />
-        <h4>Member #{index + 1}</h4>
-        <Field
-          name={`${fieldName}.firstName`}
-          type="text"
-          component={renderField}
-          label="First Name"
-        />
-        <Field
-          name={`${fieldName}.lastName`}
-          type="text"
-          component={renderField}
-          label="Last Name"
-        />
-      </li>
-    ))}
+    {fields.map((fieldName, index) => {
+      console.log(fieldName);
+      return (
+        <li key={index}>
+          <button
+            type="button"
+            title="Remove Member"
+            onClick={() => fields.remove(index)}
+          />
+          <h4>Member #{index + 1}</h4>
+          <Field
+            name={`${fieldName}.firstName`} //fieldName = members[i] it helps me to write the value in the correct child
+            type="text"
+            component={renderField}
+            label="First Name"
+          />
+          <Field
+            name={`${fieldName}.lastName`}
+            type="text"
+            component={renderField}
+            label="Last Name"
+          />
+        </li>)
+    })}
   </ul>
 );
 
